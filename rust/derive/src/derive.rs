059980c5ad75f05bb9911bd14d8ab5892f3496ae
Dr Maxim Orlovsky <orlovsky@ubideco.org>
2023-01-28 12:14:12 +0100

    derive: WIP on derivation implementations

--- rust/derive/src/derive.rs
+++ rust/derive/src/derive.rs
@@ -22,17 +22,25 @@
 use std::collections::HashMap;
 
 use amplify_syn::{ArgValueReq, AttrReq, ParametrizedAttr, TypeClass, ValueClass};
-use proc_macro2::TokenStream as TokenStream2;
+use proc_macro2::{Span, TokenStream as TokenStream2};
+use quote::ToTokens;
 use syn::spanned::Spanned;
-use syn::{Attribute, DeriveInput, Expr, LitStr, Path, Result};
+use syn::{
+    Attribute, Data, DataEnum, DataStruct, DeriveInput, Error, Expr, LitInt, LitStr, Path, Result,
+};
 
-static ATTR: &str = "strict_type";
-static ATTR_CRATE: &str = "crate";
-static ATTR_LIB: &str = "lib";
-static ATTR_RENAME: &str = "rename";
-static ATTR_WITH: &str = "with";
-static ATTR_ENCODE_WITH: &str = "encode_with";
-static ATTR_DECODE_WITH: &str = "decode_with";
+const ATTR: &str = "strict_type";
+const ATTR_CRATE: &str = "crate";
+const ATTR_LIB: &str = "lib";
+const ATTR_RENAME: &str = "rename";
+const ATTR_WITH: &str = "with";
+const ATTR_ENCODE_WITH: &str = "encode_with";
+const ATTR_DECODE_WITH: &str = "decode_with";
+const ATTR_DUMB: &str = "dumb";
+const ATTR_TAGS: &str = "tags";
+const ATTR_TAGS_ORDER: &str = "order";
+const ATTR_TAGS_REPR: &str = "repr";
+const ATTR_TAGS_CUSTOM: &str = "custom";
 
 struct ContainerAttr {
     pub strict_crate: Path,
@@ -42,16 +50,29 @@ struct ContainerAttr {
     pub decode_with: Option<Path>,
 }
 
+struct EnumAttr {
+    pub tags: VariantTags,
+}
+
 struct FieldAttr {
     pub dumb: Option<Expr>,
+    pub rename: Option<LitStr>,
 }
 
 struct VariantAttr {
     pub dumb: bool,
+    pub rename: Option<LitStr>,
+    pub value: Option<LitInt>,
+}
+
+enum VariantTags {
+    Repr,
+    Order,
+    Custom,
 }
 
 impl TryFrom<&[Attribute]> for ContainerAttr {
-    type Error = syn::Error;
+    type Error = Error;
 
     fn try_from(attr: &[Attribute]) -> Result<Self> {
         let map = HashMap::from_iter(vec![
@@ -81,13 +102,47 @@ impl TryFrom<&[Attribute]> for ContainerAttr {
     }
 }
 
+impl TryFrom<&[Attribute]> for EnumAttr {
+    type Error = Error;
+
+    fn try_from(attr: &[Attribute]) -> Result<Self> {
+        let map = HashMap::from_iter(vec![(ATTR_TAGS, ArgValueReq::required(TypeClass::Path))]);
+
+        let mut params = ParametrizedAttr::with(ATTR, &attr)?;
+        params.check(AttrReq::with(map))?;
+
+        let tags = match params
+            .arg_value(ATTR_TAGS)
+            .unwrap_or_else(|_| path!(custom))
+            .to_token_stream()
+            .to_string()
+            .as_str()
+        {
+            ATTR_TAGS_REPR => VariantTags::Repr,
+            ATTR_TAGS_ORDER => VariantTags::Order,
+            ATTR_TAGS_CUSTOM => VariantTags::Custom,
+            unknown => {
+                return Err(Error::new(
+                    Span::call_site(),
+                    format!(
+                        "invalid enum strict encoding value for `tags` attribute `{unknown}`; \
+                         only `repr`, `order` or `custom` are allowed"
+                    ),
+                ))
+            }
+        };
+
+        Ok(EnumAttr { tags })
+    }
+}
+
 pub struct StrictDerive {
     input: DeriveInput,
     conf: ContainerAttr,
 }
 
 impl TryFrom<DeriveInput> for StrictDerive {
-    type Error = syn::Error;
+    type Error = Error;
 
     fn try_from(input: DeriveInput) -> Result<Self> {
         let conf = ContainerAttr::try_from(input.attrs.as_ref())?;
@@ -97,9 +152,41 @@ impl TryFrom<DeriveInput> for StrictDerive {
 
 impl StrictDerive {
     pub fn strict_dumb(&self) -> Result<TokenStream2> {
-        let (impl_generics, ty_generics, where_clause) = self.input.generics.split_for_impl();
-        Ok(quote_spanned! { self.input.span() =>
+        match &self.input.data {
+            Data::Struct(data) => self.strict_dumb_struct(data),
+            Data::Enum(data) => self.strict_dumb_enum(data),
+            Data::Union(_) => Err(Error::new_spanned(
+                &self.input,
+                "deriving strict type traits is not supported in unions".to_owned(),
+            )),
+        }
+    }
+}
 
+impl StrictDerive {
+    fn strict_dumb_struct(&self, data: &DataStruct) -> Result<TokenStream2> {
+        let (impl_generics, ty_generics, where_clause) = self.input.generics.split_for_impl();
+
+        let import = &self.conf.strict_crate;
+        let ident_name = &self.input.ident;
+
+        Ok(quote_spanned! { self.input.span() =>
+            impl #impl_generics #import::StrictDumb for #ident_name #ty_generics #where_clause {
+
+            }
+        })
+    }
+
+    fn strict_dumb_enum(&self, data: &DataEnum) -> Result<TokenStream2> {
+        let (impl_generics, ty_generics, where_clause) = self.input.generics.split_for_impl();
+
+        let import = &self.conf.strict_crate;
+        let ident_name = &self.input.ident;
+
+        Ok(quote_spanned! { self.input.span() =>
+            impl #impl_generics #import::StrictDumb for #ident_name #ty_generics #where_clause {
+
+            }
         })
     }
 }
