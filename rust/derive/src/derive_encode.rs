cdb05dae64c1acd61162272521d98b769d35679b
Dr Maxim Orlovsky <orlovsky@ubideco.org>
2023-01-29 13:33:32 +0100

    derive: impl StrictEncode for enum types

--- rust/derive/src/derive_encode.rs
+++ rust/derive/src/derive_encode.rs
@@ -19,7 +19,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use amplify_syn::{DeriveInner, Field, Items, NamedField, Variant};
+use amplify_syn::{DeriveInner, Field, Fields, Items, NamedField, Variant};
 use proc_macro2::{Ident, Span, TokenStream as TokenStream2};
 use syn::{Error, Index, LitStr, Result};
 
@@ -33,18 +33,6 @@ impl StrictDerive {
     }
     pub fn derive_decode(&self) -> Result<TokenStream2> { Ok(quote! {}) }
 }
-
-/*
-impl DeriveEncode<'_> {
-    fn derive_newtype(&self, field: &Field) -> Result<TokenStream2> {
-        Ok(quote! {
-            fn strict_encode<W: TypedWrite>(&self, writer: W) -> ::std::io::Result<W> {
-                writer.write_newtype::<Self>(self.0)
-            }
-        })
-    }
-}
- */
 
 impl DeriveInner for DeriveEncode<'_> {
     fn derive_unit_inner(&self) -> Result<TokenStream2> {
@@ -96,12 +84,103 @@ impl DeriveInner for DeriveEncode<'_> {
         })
     }
 
-    fn derive_enum_inner(&self, fields: &Items<Variant>) -> Result<TokenStream2> {
+    fn derive_enum_inner(&self, variants: &Items<Variant>) -> Result<TokenStream2> {
         let crate_name = &self.0.conf.strict_crate;
+
+        let inner = if variants.iter().all(|var| var.fields.is_unit()) {
+            quote! {
+                writer.write_enum(*self)
+            }
+        } else {
+            let mut define_fields = Vec::with_capacity(variants.len());
+            let mut write_fields = Vec::with_capacity(variants.len());
+            for var in variants {
+                let attr = VariantAttr::try_from(var.attr.clone()).expect("invalid attribute");
+                let var_name = &var.name;
+                let name = match attr.rename.as_ref() {
+                    None => var_name, // TODO: Lowercase the first letter
+                    Some(name) => name,
+                };
+                let name = LitStr::new(&name.to_string(), Span::call_site());
+                match &var.fields {
+                    Fields::Unit => {
+                        define_fields.push(quote! {
+                            .define_unit(fname!(#name))
+                        });
+                        write_fields.push(quote! {
+                            Self::#var_name => writer.write_unit(fname!(#name))?,
+                        });
+                    }
+                    Fields::Unnamed(fields) => {
+                        let mut field_ty = Vec::with_capacity(fields.len());
+                        let mut field_idx = Vec::with_capacity(fields.len());
+                        for (index, field) in fields.iter().enumerate() {
+                            let ty = &field.ty;
+                            let index = Ident::new(&format!("_{index}"), Span::call_site());
+                            field_ty.push(quote! { #ty });
+                            field_idx.push(quote! { #index });
+                        }
+                        define_fields.push(quote! {
+                            .define_tuple(fname!(#name), |d| {
+                                d #( .define_field::<#field_ty>() )* .complete()
+                            })
+                        });
+                        write_fields.push(quote! {
+                            Self::#var_name( #( #field_idx ),* ) => writer.write_tuple(fname!(#name), |w| {
+                                Ok(w #( .write_field(#field_idx)? )* .complete())
+                            })?
+                        });
+                    }
+                    Fields::Named(fields) => {
+                        let mut field_ty = Vec::with_capacity(fields.len());
+                        let mut field_name = Vec::with_capacity(fields.len());
+                        let mut field_rename = Vec::with_capacity(fields.len());
+                        for named_field in fields {
+                            let attr = FieldAttr::try_from(named_field.field.attr.clone())?;
+
+                            let ty = &named_field.field.ty;
+                            let name = &named_field.name;
+                            let rename = match attr.rename {
+                                None => named_field.name.clone(),
+                                Some(name) => name,
+                            };
+                            field_ty.push(quote! { #ty });
+                            field_name.push(quote! { #name });
+                            field_rename.push(quote! { #rename });
+                        }
+
+                        define_fields.push(quote! {
+                            .define_struct(fname!(#name), |d| {
+                                d #( .define_field::<#field_ty>() )* .complete()
+                            })
+                        });
+                        write_fields.push(quote! {
+                            Self::#var_name { #( #field_name ),* } => writer.write_struct(fname!(#name), |w| {
+                                Ok(w #( .write_field(fname!(stringify!(#field_rename)), #field_name)? )* .complete())
+                            })?
+                        });
+                    }
+                }
+            }
+
+            quote! {
+                use ::#crate_name::{DefineUnion, WriteUnion, DefineTuple, DefineStruct};
+                writer.write_union::<Self>(|definer| {
+                    let writer = definer
+                        #( #define_fields )*
+                        .complete();
+
+                    Ok(match self {
+                        #( #write_fields )*
+                    }.complete());
+                })
+            }
+        };
 
         Ok(quote! {
             fn strict_encode<W: ::#crate_name::TypedWrite>(&self, writer: W) -> ::std::io::Result<W> {
-                todo!()
+                use ::#crate_name::TypedWrite;
+                #inner
             }
         })
     }
