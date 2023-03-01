6edb1430c173ed5a4fc3d9bc824bfaf3a9632843
Dr Maxim Orlovsky <orlovsky@ubideco.org>
2023-03-01 12:00:15 +0100

    util: fix display of variant tags

--- rust/src/util.rs
+++ rust/src/util.rs
@@ -115,7 +115,7 @@ impl Display for Variant {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.name)?;
         if f.alternate() {
-            f.write_str(" = ")?;
+            f.write_str(":")?;
             Display::fmt(&self.tag, f)?;
         }
         Ok(())
